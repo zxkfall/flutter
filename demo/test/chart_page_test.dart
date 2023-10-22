247d393183d47e86d94891f81f352418cf994a93
xingkun.zhang <1475795322@qq.com>
2023-10-23 01:24:09 +0800

    test: add chart page test

---
+++ demo/test/chart_page_test.dart
@@ -0,0 +1,37 @@
+import 'package:demo/page/chart_page.dart';
+import 'package:demo/provider/billing_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+
+void main(){
+
+  testWidgets('should show empty chart page', (widgetTester) async {
+    await widgetTester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<BillingProvider>(
+            create: (_) => BillingProvider(),
+          ),
+        ],
+        child: const MaterialApp(
+            home: Scaffold(
+              body: Directionality(
+                textDirection: TextDirection.rtl,
+                child: SingleChildScrollView(
+                  child: ChartPage(),
+                ),
+              ),
+            ),
+        ),
+      ),
+    );
+    await widgetTester.pump();
+
+    expect(find.text('Week'), findsOneWidget);
+    expect(find.text('Month'), findsOneWidget);
+    expect(find.text('Year'), findsOneWidget);
+    expect(find.text('Expense'), findsOneWidget);
+
+  });
+}
