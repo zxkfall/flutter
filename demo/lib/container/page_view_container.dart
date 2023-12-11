2e20e4708a431ec7f65e09f2d4a74f4d41eb636d
xingkun.zhang <1475795322@qq.com>
2023-12-11 11:29:04 +0800

    feat: temp minimize toolbar height

--- demo/lib/container/page_view_container.dart
+++ demo/lib/container/page_view_container.dart
@@ -44,7 +44,7 @@ class _PageViewContainerState extends State<PageViewContainer> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.onSurfaceVariant,
-          toolbarHeight: currentPage == 1 ? 0 : null,
+          toolbarHeight: 0,
         ),
         body: PageView(
           physics: const NeverScrollableScrollPhysics(),
