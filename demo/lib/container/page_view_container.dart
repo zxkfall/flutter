017dfe7143f4dd256ba7da1ec5d5f15d2acad9f3
xingkun.zhang <1475795322@qq.com>
2023-12-12 14:20:43 +0800

    refactor: remove useless code

--- demo/lib/container/page_view_container.dart
+++ demo/lib/container/page_view_container.dart
@@ -238,25 +238,6 @@ class _PageViewContainerState extends State<PageViewContainer> {
   void _goToBillingDetailPage(BuildContext context) {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (_) => const BillingDetailPage()));
-    // not support swipe back
-    // Navigator.of(context).push(
-    //   PageRouteBuilder(
-    //     pageBuilder: (context, animation, secondaryAnimation) =>
-    //         const BillingDetailPage(),
-    //     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-    //       const begin = Offset(1.0, 0.0);
-    //       const end = Offset.zero;
-    //       const curve = Curves.easeInOut;
-    //       var tween =
-    //           Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-    //       var offsetAnimation = animation.drive(tween);
-    //       return SlideTransition(
-    //         position: offsetAnimation,
-    //         child: child,
-    //       );
-    //     },
-    //   ),
-    // );
   }
 
   Future<List<Billing>> _loadBillingData() async {
